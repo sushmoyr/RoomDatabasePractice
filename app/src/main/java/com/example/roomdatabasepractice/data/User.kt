44a71232c336d0d46df8f83f2aa1c6fc4932a58b
Sushmoy Roy <34061341+Ex-Mach1na@users.noreply.github.com>
2021-01-18 03:04:23 +0600

    Database Schema Created

--- app/src/main/java/com/example/roomdatabasepractice/data/User.kt
+++ app/src/main/java/com/example/roomdatabasepractice/data/User.kt
@@ -1,5 +1,14 @@
 package com.example.roomdatabasepractice.data
 
-class User {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-}
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+    var firstName: String,
+    var lastName: String,
+    var age: Int
+
+)
